2201e1d1095e1546b758c823e3f0b5a6ac3d8738
aydoran <aydoran@iu.edu>
2026-06-01 17:09:03 -0400

    Create BookingController.php

---
+++ api/Controllers/BookingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace courseProj\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use courseProj\Models\Booking;
+use courseProj\Controllers\ControllerHelper as Helper;
+
+class BookingController
+{
+    public function index(Request $request, Response $response, array $args): Response
+    {
+        $results = Booking::getBookings();
+        return Helper::withJson($response, $results, 200);
+    }
+
+    public function view(Request $request, Response $response, array $args): Response
+    {
+        $id = (int)$args['id'];
+        $results = Booking::getBookingById($id);
+        return Helper::withJson($response, $results, 200);
+    }
+}
