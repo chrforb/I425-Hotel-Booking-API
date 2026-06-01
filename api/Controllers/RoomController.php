f2be7d63494cc3d525ceec3b175ea905254b8ab6
aydoran <aydoran@iu.edu>
2026-06-01 17:08:44 -0400

    Create RoomController.php

---
+++ api/Controllers/RoomController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace courseProj\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use courseProj\Models\Room;
+use courseProj\Controllers\ControllerHelper as Helper;
+
+class RoomController
+{
+    public function index(Request $request, Response $response, array $args): Response
+    {
+        $results = Room::getRooms();
+        return Helper::withJson($response, $results, 200);
+    }
+
+    public function view(Request $request, Response $response, array $args): Response
+    {
+        $id = (int)$args['id'];
+        $results = Room::getRoomById($id);
+        return Helper::withJson($response, $results, 200);
+    }
+}
