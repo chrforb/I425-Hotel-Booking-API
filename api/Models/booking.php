615916ecf4414c5123c6acb09e4a0c42f5776a0f
annltodd <annltodd@iu.edu>
2026-06-01 10:06:02 -0400

    Add methods to retrieve bookings and booking by ID

--- api/Models/booking.php
+++ api/Models/booking.php
@@ -1,3 +1,12 @@
 public function amenities() {
     return $this->belongsToMany(Amenity::class, 'booking_amenity', 'booking', 'amenity');
 }
+public static function getBookings() {
+    $bookings = self::with('amenities')->get();
+    return $bookings;
+}
+public static function getBookingById(string $id) {
+    $booking = self::findOrFail($id);
+    $booking->load('amenities');
+    return $booking;
+}
