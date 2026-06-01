364b62cac309b28248eb915c9b117b0e292f06f6
annltodd <annltodd@iu.edu>
2026-06-01 10:06:38 -0400

    Add method to get amenities by booking ID

--- api/Models/amenity.php
+++ api/Models/amenity.php
@@ -1,3 +1,8 @@
 public function bookings() {
     return $this->belongsToMany(Booking::class, 'booking_amenity', 'amenity', 'booking');
 }
+
+public static function getAmenitiesByBooking(string $id) {
+    $amenities = self::findOrFail($id)->amenities;
+    return $amenities;
+}
