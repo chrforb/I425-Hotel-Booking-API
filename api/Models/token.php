53be7d11debe9ad9ed1a7cfa3e5d88091b20728b
annltodd <annltodd@iu.edu>
2026-06-15 15:51:32 -0400

    Implement Token model with bearer token methods

--- api/Models/token.php
+++ api/Models/token.php
@@ -1,1 +1,36 @@
+<?php 
 
+namespace I425-Hotel-Booking-API/api/Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+  {
+    protected $table = 'tokens';
+    protected $primarykey = 'id';
+    public $timestamps = false;
+
+    const EXPIRE = 3600;
+
+    public statix function generateBearer(int $user_id)
+    { 
+      $token = bin2hex(random_bytes(16));
+      $bearer = new Token();
+
+      $bearer->user_id = $user_id;
+      $bearer->token = $token;
+      $bearer->expires = time() + self::EXPIRE;
+
+      $bearer->save();
+
+      return $token;
+
+    }
+
+    public static function validateBearer(string $token)
+    {
+      $bearer = self::where('token', $token)
+        ->where('expires', '>', time())
+        ->first();
+      return $bearer;
+    }
+  }
