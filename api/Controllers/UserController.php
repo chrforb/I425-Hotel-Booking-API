7e6c96167c94a4f29d51a18461e9f80e298b5bab
annltodd <annltodd@iu.edu>
2026-06-15 16:24:43 -0400

    Implement Bearer token authentication method

--- api/Controllers/UserController.php
+++ api/Controllers/UserController.php
@@ -1,1 +1,32 @@
+<?php
+use courseProj\Models\Token;
 
+public function authBearer(Request $request, Response $response, array $args): Response
+{
+    // Retrieve username and password
+    $params = $request->getParsedBody();
+
+    $username = $params['username'];
+    $password = $params['password'];
+
+    // Verify username and password
+    $user = User::authenticateUser($username, $password);
+
+    if (!$user) {
+        return Helper::withJson(
+            $response,
+            ['Status' => 'Login failed.'],
+            401
+        );
+    }
+
+    // Generate token
+    $token = Token::generateBearer($user->id);
+
+    $results = [
+        'Status' => 'Login successful',
+        'Token' => $token
+    ];
+
+    return Helper::withJson($response, $results, 200);
+}
