1fbdef8292d79adc4b09f783266c3bd94f5414cb
annltodd <annltodd@iu.edu>
2026-06-15 15:30:01 -0400

    Fix user authentication method to use password hashing

--- api/Models/user.php
+++ api/Models/user.php
@@ -1,1 +1,21 @@
+<?php
 
+public static function authenticateUser(string $username, string $password)
+{
+    $user = self::where('username', $username)
+                ->where('password', $password)
+                ->first();
+
+    return $user !== null;
+}
+
+public static function authenticateUser(string $username, string $password)
+{
+    $user = self::where('username', $username)->first();
+
+    if (!$user) {
+        return false;
+    }
+
+    return password_verify($password, $user->password);
+}
